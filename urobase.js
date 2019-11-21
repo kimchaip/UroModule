8f80f215eebdbfa1baafcf3c7fb837042322e6c8
kimchaip <kim_chaip@hotmail.com>
2019-11-21 19:33:08 +0700

    Update urobase.js

--- urobase.js
+++ urobase.js
@@ -448,7 +448,7 @@ var que = {
     if(this.fnd != "" &​& this.q.length > 0) {
       this.q.find(function(ent) {
         return ent.field("Que")​ == this.fnd;
-      }​);
+      }, this​);
     }​
   }, 
   sort : function()​ {
