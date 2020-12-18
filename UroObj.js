23272f56e56fbba453ae86070ddec65353af5f81
kimchaip <kim_chaip@hotmail.com>
2020-12-18 20:31:51 +0700

    Update UroObj.js
    correct error age

--- UroObj.js
+++ UroObj.js
@@ -995,7 +995,7 @@ var pto = {
         e.set("Age", e.field("YY")​ + " ปี")​;
       }​
       else if (e.field("Birthday")​ != null)​ {
-        d = Math.Floor((ntoday-my.gdate(e.field("Birthday"))​)/86400000/365);
+        d = Math.Floor((ntoday-my.gdate(e.field("Birthday"))​)/86400000);
         this.agetext(e, d)​;
       }​
     }​
