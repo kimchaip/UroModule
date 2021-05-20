94c275337a7615c12efb46004fb86423a912a44c
kimchaip <kim_chaip@hotmail.com>
2021-05-20 17:33:37 +0700

    Update UroObj.js
    correct error

--- UroObj.js
+++ UroObj.js
@@ -1137,9 +1137,9 @@ var uro = {
       let changedj = opresult.match(/change|dj/ig)​;
       let opchange = e.field("Op").match(/change|dj/ig)​;
       if(e.field("Status")=="Plan") e.set("Status", "Done")​;
-      if(ondj!=null&&ondj.length>1||opon!=null&&opon.length>0) e.set("DJstent", "on DJ");
-      else if(changedj!=null&&changedj.length>1​||opchange!=null&&opchange.length>1) e.set("DJstent", "change DJ");
-      else if(offdj!=null&&offdj.length>1||opoff!=null&&opoff.length>1) e.set("DJstent", "off DJ");
+      if((ondj!=null&&ondj.length>1)||(opon!=null&&opon.length>0)) e.set("DJstent", "on DJ");
+      else if((changedj!=null&&changedj.length>1)||(opchange!=null&&opchange.length>1)) e.set("DJstent", "change DJ");
+      else if((offdj!=null&&offdj.length>1)||(opoff!=null&&opoff.length>1)) e.set("DJstent", "off DJ");
     }
   }, 
   runq : function (e) {
