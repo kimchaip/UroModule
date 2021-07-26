6b7f4ac00bb73385bf126d5be6e84d5611fe16c2
kimchaip <kim_chaip@hotmail.com>
2021-07-26 18:03:44 +0700

    Update UroObj.js
    correct error

--- UroObj.js
+++ UroObj.js
@@ -1986,7 +1986,6 @@ var trig = {
     fill.track​(e, "uro")​;
     if(e.field("Que")​!=oldUr.que &​& e.field("ORType")​=="GA" &​& e.field("Status") != "Not") {
       uro.runq(e)​;
-      oldUr.save(e)​;
     }
   }, 
   UroAfterUpdatingField : function (e) {
