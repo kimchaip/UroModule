f61b0998ae84d5639eda8f1639c5e63b827582f8
kimchaip <kim_chaip@hotmail.com>
2021-11-27 16:59:42 +0700

    Update UroObj.js
    debug turn on

--- UroObj.js
+++ UroObj.js
@@ -453,7 +453,7 @@ var que = {
     if (my.gdate(e.field("TimeIn")) != my.gdate(old.field("TimeIn")) || e.field("Que") != old.field("Que") || e.field("Status") != old.field("Status") || e.field("ORType") != old.field("ORType")) {
       // load old entry to q
       this.load(e);
-      //e.set("Output", q.map(v=>v.field("Que")).join());
+      e.set("Output", "load old:"+q.map(v=>v.field("Que")).join());
       // remove old e or insert new e
       if (e.field("Status") == "Not" || e.field("ORType")​ == "LA") {  // change Status -> Not or ORType -> LA
         this.remove(e);
@@ -462,19 +462,19 @@ var que = {
       else {  // change Status -> !Not and ORType -> GA
         this.insert(e);
       }
-      //e.set("Output", e.field("Output")+"\n"+q.map(v=>v.field("Que")).join());
+      e.set("Output", e.field("Output")+"\nto new:"+q.map(v=>v.field("Que")).join());
       // sort q by que
       this.sortque(e);
-      //e.set("Output", e.field("Output")+"\n"+q.map(v=>v.field("Que")).join());
+      e.set("Output", e.field("Output")+"\nsort que:"+q.map(v=>v.field("Que")).join());
       // update when Que change and !Not and GA
       if (e.field("Que") != old.field("Que") && e.field("Status") != "Not" && e.field("ORType")​ == "GA") {
         // insert this entry to q at position que
         this.insert(e);
       }
-      //e.set("Output", e.field("Output")+"\n"+q.map(v=>v.field("Que")).join());
+      e.set("Output", e.field("Output")+"\nupdate que:"+q.map(v=>v.field("Que")).join());
       //reorder by TimeIn -> set new que to every entry
       this.save(e);
-      //e.set("Output", e.field("Output")+"\n"+q.map(v=>v.field("Que")).join());
+      e.set("Output", e.field("Output")+"\nsort time and save:"+q.map(v=>v.field("Que")).join());
     }
   }
 };
