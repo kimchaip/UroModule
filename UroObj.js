e411dc861f4aae1b8da6a5528747d016c9e68863
kimchaip <kim_chaip@hotmail.com>
2022-01-13 10:19:42 +0700

    Update UroObj.js
    correct error

--- UroObj.js
+++ UroObj.js
@@ -898,7 +898,7 @@ var fill = {
         if (o.e.field("Dx")!="")​
           str = e.field("Dx");
 
-        let lib;
+        var lib;
         if(o.lib=="UroBase") {
           lib = uro;
         }
