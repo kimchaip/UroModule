47c8268aaa7177fb40e8f30a4e0311bdb3f2da69
kimchaip <kim_chaip@hotmail.com>
2022-07-01 18:29:06 +0700

    Update UroObj.js
    correct error

--- UroObj.js
+++ UroObj.js
@@ -1677,7 +1677,7 @@ var opu = {
             let underly = link.field("Underlying").join().toLowerCase();
             note = note.map(v=>v.trim());
             note = note.filter(v=>underly.indexOf(v.trim().toLowerCase())==-1);
-            note = link.field("Underlying").concat(note);
+            //note = link.field("Underlying").concat(note);
             oss[s].set("Note", note.join(", "));
             
             if(!oss[s].field("CreationTime"))
