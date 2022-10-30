65ff2b8e8d7962f809a5c9c2d12c14c176574901
kimchaip <kim_chaip@hotmail.com>
2022-10-30 13:50:00 +0700

    Update UroObj.js
    Cert

--- UroObj.js
+++ UroObj.js
@@ -1856,8 +1856,8 @@ var rpo = {
         if (rps.length>0) {
           for (let r=0; r<rps.length; r++)​ {
             if (my.gdate(my.date(rps[r].field("OpDate"))​) == my.gdate(my.date(old.field("Date"))​) && rps[r].field("ORType") ==​ old.field("ORType") &​& rps[r].field("Dx") ==​ old.field("Dx") &​& rps[r].field("Op") ==​ old.field("Op"))​{
-              rpo.setAll(rps[r], e);
               str+="\n"+ rps[r].field("Dx")+" ==​ "+old.field("Dx")+" &​& "+rps[r].field("Op")+" ==​ "+old.field("Op");
+              rpo.setAll(rps[r], e);
               break;
             }
           }​​
