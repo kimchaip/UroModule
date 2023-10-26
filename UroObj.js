3e43d3f3a6ca387c5d82cf9a53e46604deb19997
kimchaip <kim_chaip@hotmail.com>
2023-10-26 15:57:27 +0700

    Update UroObj.js
    correct error mer.cancel

--- UroObj.js
+++ UroObj.js
@@ -254,7 +254,7 @@ var mer = {
           e.set("VisitDate", my.dateminus(e.field("ConsultDate"), 1));
         }
         if (my.gdate(e.field("VisitDate"))>ntoday) {
-          e.set("Ward", e.field("VisitType")=="Admit"?Trauma9:"OPD");
+          e.set("Ward", e.field("VisitType")=="Admit"?"Trauma9":"OPD");
           e.set("DischargeDate", null);
           e.set("Track", 0);
           e.set("Summary", false);
