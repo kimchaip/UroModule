bc7cce9810920b0a4bb79eb9a714a591b43ad919
kimchaip <kim_chaip@hotmail.com>
2023-10-26 19:29:37 +0700

    Update UroObj.js
    Correct error at old.load that string Date can't converse to date

--- UroObj.js
+++ UroObj.js
@@ -13,7 +13,7 @@ var old = {
       //get Previous to Obj
       this.d = JSON.parse(e.field("Previous"), function (key, value) {
         if (value) {
-          if (typeof value == "string" && value.match(/^\d{4}-\d{2}-\d{2}$/)) {
+          if (typeof value == "string" && value.match(/^\d{4}-\d{2}-\d{2}/)) {
             return new Date(value);
           }
           else if (typeof value == "object" && key == "Underlying" || key == "Allergies" || key == "Photo") {
