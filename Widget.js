0585226fbe159bfb7e7937007ad3c5b45c50fc87
kimchaip <kim_chaip@hotmail.com>
2026-06-15 08:49:22 +0700

    Fix return statement for ui layout in Widget.js

--- Widget.js
+++ Widget.js
@@ -5,7 +5,7 @@ var widget = {
     // get the number of entries in the library
     var entryCount = lb.entries().length;
     // return the entry count as a string
-    ui().layout([
+    return ui().layout([
       ui().text("Header"),
       ui().text("Total Entries: " + entryCount),
       ui().text("end")
