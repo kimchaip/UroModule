2e3506038fa399c4f12936b3c2ae0c75da975ac9
kimchaip <kim_chaip@hotmail.com>
2026-08-02 14:59:32 +0700

    Refactor OpDiff calculation logic in UroObj.js

--- UroObj.js
+++ UroObj.js
@@ -1378,7 +1378,7 @@ var fill = {
       o = o.filter(v=>v.lib!="Consult" && v.e.field("Status")!="Not");
       n = n.filter(v=>v.lib!="Consult" && v.e.field("Status")!="Not");
       if(o.length>0 && my.compDate(ptent.field("WardStamp"), today) != more && ((ptent.field("LastDischarge")==null) || my.compDate(today, ptent.field("LastDischarge") != less)) ) { // admit or OPD OR LA
-        ptent.set("OpDiff", my.diffDate(o[0].e.field("Date"), today).day * -1);
+        ptent.set("OpDiff", my.diffDate(o[0].e.field("Date"), today).day * (my.compDate(o[0].e.field("Date"), today) == less ? -1 : 1));
       }
       else if(n.length>0) { // found next visit
         ptent.set("OpDiff", my.diffDate(n[0].e.field("Date"), today).day);
